10af06d705179682300e5bccba12260306c9f753
EC2 Default User <ec2-user@ip-172-31-37-71.eu-west-2.compute.internal>
2022-04-17 22:07:43 +0000

    e

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['x20180837xDjangoxDevops-env.eba-pgppr2b4.eu-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
